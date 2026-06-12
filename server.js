c472d5cbd2a766a5c26122a541db40d184635fd9
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2026-06-12 10:20:27 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'UP', message: 'Application is healthy', version: '1.0.0' });
+});
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Enterprise CI/CD Pipeline Successful!</h1>');
+});
+
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+module.exports = server; // Exported for unit testing
