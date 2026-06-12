faf21c5235bc632dc0374977f8456c6a959362b5
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2026-06-12 06:51:54 +0530

    Update 1.jenkins.sh

--- 1.jenkins.sh
+++ 1.jenkins.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install openjdk-17-jre -y
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install jenkins -y
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
-# Retrieve initial admin password
-sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+sudo apt update
+sudo apt install fontconfig openjdk-21-jre -y
+java -version
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins -y
